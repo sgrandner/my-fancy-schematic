523a2e1a8cfd48809c93d736dc8990703737a5fd
Stefan Grandner <sgrandner@gmx.de>
2023-11-02 15:24:23 +0100

    test log output, npm start script, npm build:watch script

--- src/my-fancy-schematic/index.ts
+++ src/my-fancy-schematic/index.ts
@@ -3,6 +3,9 @@ import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function myFancySchematic(_options: any): Rule {
+
+    console.log('asdfas fasdf as');
+
     return (tree: Tree, _context: SchematicContext) => {
         return tree;
     };
