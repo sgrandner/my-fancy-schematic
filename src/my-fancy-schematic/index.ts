364d6c1694fe5116abd418501d22fd11a4abec4e
Stefan Grandner <sgrandner@gmx.de>
2023-11-03 22:14:35 +0100

    add type definition for schema options

--- src/my-fancy-schematic/index.ts
+++ src/my-fancy-schematic/index.ts
@@ -1,8 +1,9 @@
 import { Rule, SchematicContext, Tree, externalSchematic } from '@angular-devkit/schematics';
+import { MyFancyOptionsSchema } from './schema';
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-export function myFancySchematic(_options: any): Rule {
+export function myFancySchematic(_options: MyFancyOptionsSchema): Rule {
 
     const fancy = _options.fancy;
 
